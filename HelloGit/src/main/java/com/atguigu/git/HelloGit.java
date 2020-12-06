1cf8e3506ed884cb15c525010a56bbd2a3fc79d4
andy-sqy <goodMorning_glb@atguigu.com>
2020-12-06 16:40:56 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+
+
+/**
+ * @author shiqy
+ * @create 2020-12-06 16:25
+ */
+public class HelloGit {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello Git!");
+
+    }
+}
